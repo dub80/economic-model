e045dd9341f5e306c996b7da73d10b542130325d
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-09-22 14:09:40 +0100

    test: add valgrind to detect memory leaks

--- src/simulation.c
+++ src/simulation.c
@@ -5,7 +5,8 @@
 simulation *_simulation;
 person **_people;
 
-simulation *initialiseSimulation(int year, person **people) {
+simulation *initialiseSimulation(int year, person **people)
+{
   _simulation = malloc(sizeof(simulation));
   _simulation->year = year;
   _people = people;
@@ -17,26 +18,49 @@ simulation *getSimulation() { return _simulation; }
 
 person **getPeople() { return _people; }
 
-void simulateYear() {
+void simulateYear()
+{
   _simulation->year += 1;
   int peopleCount = countPeople(_people);
-  for (int i = 0; i < peopleCount; i++) {
+  for (int i = 0; i < peopleCount; i++)
+  {
     personTick(_simulation->year, _people[i]);
   }
 }
 
-void simulateYears(int years) {
-  for (int i = 0; i < years; i++) {
+void simulateYears(int years)
+{
+  for (int i = 0; i < years; i++)
+  {
     simulateYear();
   }
 }
 
-void clearSimulation() {
-  if (_simulation != NULL) {
-    free(_simulation);
+void clearSimulation()
+{
+  if (_people != NULL)
+  {
+    int done = 0;
+    int index = 0;
+
+    while (done == 0)
+    {
+      if (_people[index] == NULL)
+      {
+        done = 1;
+      }
+      else
+      {
+        free(_people[index]);
+        index++;
+      }
+    }
+
+    free(_people);
   }
 
-  if (_people != NULL) {
-    free(_people);
+  if (_simulation != NULL)
+  {
+    free(_simulation);
   }
 }
